2ee4e6267fc904e6dcafcd6786959bafa94c1aab
rotol2 <77chaelin@gmail.com>
2025-01-30 15:56:40 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int year = s.nextInt();
+		int changeYear = year - 543;
+		System.out.println(changeYear);
+	}
+}

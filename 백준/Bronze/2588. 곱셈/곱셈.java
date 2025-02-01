cc7faf1a294c901b54c8127fa9970674c117dc4a
rotol2 <77chaelin@gmail.com>
2025-02-01 16:00:54 +0900

    [Bronze III] Title: 곱셈, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -8,16 +8,9 @@ public class Main {
 		int oneSu = s.nextInt();
 		int twoSu = s.nextInt();
 		
-		int total;
-		int first = twoSu / 100;
-		total = twoSu % 100;
-		int second = total / 10;
-		total = twoSu % 10;
-		int third = total;
-		
-		System.out.println(oneSu*third);
-		System.out.println(oneSu*second);
-		System.out.println(oneSu*first);
+		System.out.println(oneSu*(twoSu%10));
+		System.out.println(oneSu*(twoSu%100/10));
+		System.out.println(oneSu*(twoSu/100));
 		System.out.println(oneSu*twoSu);
 	}
 }

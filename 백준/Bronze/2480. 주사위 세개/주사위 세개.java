eb018e458c259ea48f810b378605e273ed9793d9
rotol2 <77chaelin@gmail.com>
2025-02-21 05:33:17 +0900

    [Bronze IV] Title: 주사위 세개, Time: 104 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,32 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 
-		Scanner sc = new Scanner(System.in);	
-		String[] input = sc.nextLine().split(" ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int one = Integer.parseInt(st.nextToken());
+		int two = Integer.parseInt(st.nextToken());
+		int three =  Integer.parseInt(st.nextToken());
 		
-		int[] dice = new int[input.length];
-		
-		for(int i = 0; i < input.length; i++) {
-			dice[i] = Integer.parseInt(input[i]); 
-		}
-		
-		if(dice[0] == dice[1] && dice[1] == dice[2]) {
-			System.out.println(10000 + dice[0] * 1000);
-		} else if(dice[0] == dice[1] || dice[1] == dice[2] || dice[2] == dice[0]) {
-			if(dice[0] == dice[1] || dice[0] == dice[2]) {
-				System.out.println(1000 + dice[0] * 100);
-			} else {
-				System.out.println(1000 + dice[1] * 100);
-			}
+		if(one == two && two == three) {
+			System.out.println(10000 + one * 1000);
+		} else if(one == two || two == three || three == one) {
+			System.out.println(1000 + ((one == two || one == three) ? one : two) * 100);
 		} else {
-			int max = 0;
-			for(int num : dice) {
-				max = Math.max(max, num);
-			}
-			System.out.println(max*100);
+			System.out.println(Math.max(one, Math.max(two, three)) * 100);
 		}
 		
 	}

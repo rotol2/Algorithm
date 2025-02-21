325f9a1ee65b294f5ac56721ef1ea6a21da0a41f
rotol2 <77chaelin@gmail.com>
2025-02-21 20:15:55 +0900

    [Bronze V] Title: 구구단, Time: 100 ms, Memory: 14132 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -6,11 +6,10 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int n = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
 		
-		for(int i = 1; i < 10; i++) {
+		for(int i = 1; i <= 9; i++) {
 			sb.append(n).append(" * ").append(i).append(" = ").append(n*i).append("\n");
 		}
 		System.out.println(sb);

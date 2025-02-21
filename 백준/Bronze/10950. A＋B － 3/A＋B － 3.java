78908573f495aad39a77c687433bd5e7c96b1fa1
rotol2 <77chaelin@gmail.com>
2025-02-21 21:57:47 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -5,20 +5,17 @@ import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int count = Integer.parseInt(br.readLine());
-
+		
 		int i = 0;
 		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		int A;
-		int B;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int count = Integer.parseInt(br.readLine());
 
 		while (i < count) {
-			st = new StringTokenizer(br.readLine());
-			A = Integer.parseInt(st.nextToken());
-			B = Integer.parseInt(st.nextToken());
+		    StringTokenizer st = new StringTokenizer(br.readLine());
+		    int A = Integer.parseInt(st.nextToken());
+		    int B = Integer.parseInt(st.nextToken());
 			sb.append(A + B).append("\n");
 			i++;
 		}

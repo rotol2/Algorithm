43f8ffee7305751ac352bb3aa76be0c4464030b6
rotol2 <77chaelin@gmail.com>
2025-02-25 23:19:17 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 352 ms, Memory: 80796 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,33 +1,29 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-			
-		StringBuilder sb = new StringBuilder();
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int count = Integer.parseInt(br.readLine());
-		
-		int i = 1;
-		while(i <= count) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int one = Integer.parseInt(st.nextToken());
-			int two = Integer.parseInt(st.nextToken());
-			
-			sb.append(one+two).append("\n");
-			i++;
+		BufferedInputStream bis = new BufferedInputStream(System.in, 32768);
+
+		int numCases = readInt(bis);
+		BufferedOutputStream bos = new BufferedOutputStream(System.out, numCases*6);
+
+		while (numCases-- > 0) {
+			bos.write(Integer.toString(readInt(bis) + readInt(bis)).getBytes());
+			bos.write('\n');
 		}
-        
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		bw.write(sb.toString()); 
-		bw.flush();  
-		bw.close();   
-		
-		
-		
+
+		bos.flush();
+		bos.close();
+	}
+
+	static int readInt(BufferedInputStream bis) throws IOException {
+	        int n = 0;
+	        int c;
+	        while ((c = bis.read()) != ' ' && c != '\n') {
+	            n = 10 * n + c - '0';
+	        }
+	        return n;
 	}
 }

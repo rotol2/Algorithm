3662b9ab3d92e6b7b766f772a1534fec625c225e
rotol2 <77chaelin@gmail.com>
2025-03-21 02:13:04 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class Main{
-	public static void main(String[] args)  throws IOException{
+	public static void main(String[] args) throws IOException{
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,13 +14,9 @@ public class Main{
 		
 		int count = Integer.parseInt(br.readLine());
 		
-		for(int i=1; i <= count; i++) {			
-			for(int k = 1; k <= count-i; k++) {
-				sb.append(" ");
-			}			
-			for(int j = 1; j <= i; j++) {
-				sb.append("*");
-			}			
+		for(int i=1; i <= count; i++) {	
+			sb.append(" ".repeat(count-i));
+			sb.append("*".repeat(i));
 			sb.append("\n");
 		}
 		bw.write(sb.toString());		

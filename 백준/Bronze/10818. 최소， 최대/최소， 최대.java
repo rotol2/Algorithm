22f3802d37954cfba7ea22d36f5ed3aa755a0f7f
rotol2 <77chaelin@gmail.com>
2025-03-29 01:21:56 +0900

    [Bronze III] Title: 최소, 최대, Time: 432 ms, Memory: 87780 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -15,14 +15,13 @@ public class Main {
 		int count = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine());		
 		
-		int[] array = new int[count];
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
 		
 		for(int i = 0; i < count; i++) {	
-			array[i] = Integer.parseInt(st.nextToken());
-			if(max < array[i]) max = array[i];
-			if(min > array[i]) min = array[i];
+			int num = Integer.parseInt(st.nextToken());
+			max = Math.max(max, num);
+			min = Math.min(min, num);
 		}
 		sb.append(min).append(" ").append(max);
 		
